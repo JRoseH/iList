1d33e91ca3ca12eb47c9f7ccabdb8f215196202a
Null <kimtkeivn@gmail.com>
2018-03-31 17:48:47 -0700

    edited main UI and tested for listview

--- app/src/main/java/com/example/anull/todolist/MainActivity.java
+++ app/src/main/java/com/example/anull/todolist/MainActivity.java
@@ -8,13 +8,27 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    // this is just to test the code;
+    ArrayList<String> todo = new ArrayList<String>(1);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        todo.add(0,"hello");
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_listview, todo );
+
+
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
 
 
 
